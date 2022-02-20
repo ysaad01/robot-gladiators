fbace131b3b239b7dc0551e127885f1701cf5cf6
ysaad01 <ysaad2325@gmail.com>
2022-02-20 00:49:11 -0500

    used localStorage API to save our highscore

--- assets/js/game.js
+++ assets/js/game.js
@@ -160,20 +160,30 @@ var startGame = function () {
 };
 
 // function to end the entire game
-var endGame = function() {
-    // if player is still alive, player wins!
-    if (playerInfo.health > 0) {
-        window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + ".");
+var endGame = function () {
+    window.alert("The game has now ended. Let's see how you did!");
+
+    // check localStorage for highscore, if it's not there, use 0
+    var highScore = localStorage.getItem("highscore");
+    if (highScore === null) {
+        highscore = 0;
+    }
+
+    // if player has more money than the high score, player has new high score!
+    if (playerInfo.money > highScore) {
+        localStorage.setItem("highscore", playerInfo.money);
+        localStorage.setItem("name", playerInfo.name);
+
+        alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
     }
     else {
-        window.alert("The game has now ended. Let's see how you did!");
+        alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
     }
 
     // ask player if they'd like to play again
     var playAgainConfirm = window.confirm("Would you like to play again?");
 
     if (playAgainConfirm) {
-        // restart the game
         startGame();
     }
     else {
