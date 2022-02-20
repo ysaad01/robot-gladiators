09f90493ed3a8d3ab727fe9e1985262cad46063b
ysaad01 <ysaad2325@gmail.com>
2022-02-19 21:56:38 -0500

    Player name bug fixed using getPlayerName() function

--- assets/js/game.js
+++ assets/js/game.js
@@ -163,8 +163,20 @@ var shop = function() {
     }
 };
 
+// function to set name
+var getPlayerName = function () {
+    var name = "";
+    while (name === "" || name === null) {
+        name = prompt("What is your robot's name?");
+    }
+
+    console.log("Your robot's name is " + name);
+    return name;
+};
+
+/* GAME INFORMATION / VARIABLES */
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
