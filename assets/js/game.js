5c5508bfdb92dc712a5bae491547a8a5db907241
ysaad01 <ysaad2325@gmail.com>
2022-02-19 23:24:09 -0500

    updated shop prompt to accept integer responses

--- assets/js/game.js
+++ assets/js/game.js
@@ -11,7 +11,7 @@ var fightOrSkip = function () {
     var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
 
     // if the 'promptFight' is NOT a valid value, then execute the following statements. 
-    if (!promptFight) {
+    if (promptFight === "" || promptFight === null) {
         window.alert("You need to provide a valid answer! Please try again.");
         return fightOrSkip();
     }
@@ -155,22 +155,23 @@ var endGame = function() {
 var shop = function() {
     // ask player what they'd like to do 
     var shopOptionPrompt = window.prompt(
-        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
     );
+
+    // convert answer from a string to an integer
+    shopOptionPrompt = parseInt(shopOptionPrompt);
+
     // use switch to carry out action
     switch (shopOptionPrompt) {
-        case "REFILL": // new case
-        case "refill":
+        case 1:
             playerInfo.refillHealth();
             break;
 
-        case "UPGRADE": // new case
-        case "upgrade":
+        case 2:
             playerInfo.upgradeAttack();
             break;
 
-        case "LEAVE": // new case
-        case "leave": 
+        case 3: 
             window.alert("leaving the store");
 
             // do nothing, so function will end
